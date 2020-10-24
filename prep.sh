ede33cfa4adacbdf8569d699d36b384ae2f38ea9
Neil Stone <neil.stone@gmail.com>
2020-10-24 22:32:38 +0100

    prep.sh: Remove all the bashrc stuff, and just put the prompt in

--- prep.sh
+++ prep.sh
@@ -3,7 +3,6 @@
 #
 # apt update ; apt upgrade -y ; curl -4 -o /root/prep.sh https://raw.githubusercontent.com/Flashtekuk/Public/master/prep.sh; chmod +x /root/prep.sh
 
-
 ##
 # Install packages
 #
@@ -21,29 +20,9 @@ gpasswd -a neil wheel
 
 sed -i.bak -e 's/# auth       sufficient pam_wheel.so trust/auth       sufficient pam_wheel.so trust/g' /etc/pam.d/su
 
-cat << EOF > /root/.bashrc
-# ~/.bashrc: executed by bash(1) for non-login shells.
-
-# Note: PS1 and umask are already set in /etc/profile. You should not
-# need this unless you want different defaults for root.
-# PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
-# umask 022
-
-# You may uncomment the following lines if you want `ls' to be colorized:
-export LS_OPTIONS='--color=auto'
-eval "`dircolors`"
-alias ls='ls $LS_OPTIONS'
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
-#
-# Some more alias to avoid making mistakes:
-# alias rm='rm -i'
-# alias cp='cp -i'
-# alias mv='mv -i'
+echo >> /root/.bashrc
 
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[01;33m\]@\[\033[01;36m\]\h \[\033[01;33m\]\w \[\033[01;35m\]\$ \[\033[00m\]'
-
-EOF
 
 #
 # End of my personal customisations
@@ -53,5 +32,5 @@ EOF
 # Download webserver-setup.sh
 #
 curl -o /root/webserver-setup.sh https://raw.githubusercontent.com/Flashtekuk/Public/master/webserver-setup.sh
-curl -o /root/webserver-setup.sh https://raw.githubusercontent.com/Flashtekuk/Public/master/post.sh
+curl -o /root/post.sh https://raw.githubusercontent.com/Flashtekuk/Public/master/post.sh
 chmod +x /root/*.sh
