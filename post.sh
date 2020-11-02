18cb4574808b4f751bd228b059662efc6f0a5fdc
Neil Stone <neil.stone@gmail.com>
2020-11-02 23:37:57 +0000

    post.sh - added bash completion script

--- post.sh
+++ post.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 gpasswd -a neil www-data
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+curl -O https://raw.githubusercontent.com/wp-cli/wp-cli/v2.4.0/utils/wp-completion.bash
+
