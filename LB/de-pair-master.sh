f3497853a12d6c248185e52e1622f59b73d717d7
Neil Stone <neil.stone@gmail.com>
2021-04-12 15:13:06 +0100

    Initial commit of de-pair scripts

---
+++ LB/de-pair-master.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+ ###############################################################################################################
+#                                                                                                               #
+# Script to convert Loadbalancer.org HA paired master node XML config to a standalone master ready for pairing. #
+# Uses 'xmlstarlet' package. Please install prior to using this script.                                         #
+#                                                                                                               #
+# v1.0 - 2021-04-12 - Neil Stone - Initial write                                                                #
+#                                                                                                               #
+ ###############################################################################################################
+
+# Check xmlstarlet is installed
+which xmlstarlet &> /dev/null || echo "Binary: 'xmlstarlet' not found in path, please check this is installed and try again"
+
+if [ $# -ne 1 ]; then
+	CONFIG=lb_config.xml
+else
+	CONFIG=${1}
+fi
+
+if [ ! -f ${CONFIG} ]; then
+	echo "Unable to access ${CONFIG}"
+	exit 2
+fi
+
+OUT=unpaired-master-${CONFIG}
+
+xmlstarlet edit -O -P \
+-u config/physical/network/master -v "" \
+-u config/physical/network/slave  -v "" \
+\
+-u config/physical/pool/name -v "" \
+-u config/physical/pool/state -v none \
+\
+-u config/heartbeat/ucast_interface/master -v lo \
+-u config/heartbeat/ucast_interface/slave -v lo \
+-u config/heartbeat/ucast_ip/master -v 127.0.0.1 \
+-u config/heartbeat/ucast_ip/slave -v 127.0.0.1 \
+\
+-u config/global/firewall/conntrack_size -v 524288 \
+-u config/global/firewall/lockdown -v off \
+-u config/global/firewall/admin_net -v 0.0.0.0/0 \
+\
+-d config/physical/pool/node \
+-d config/physical/pool/node/hostname \
+-d config/physical/pool/node/role \
+-d config/physical/pool/node/ip \
+-d config/physical/pool/node/type \
+${CONFIG} > ${OUT}
+
+echo "Converted: ${CONFIG} to ${OUT}"
