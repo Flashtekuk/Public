661706ed5e1cd0ed5c3bc545760ab4f166dff9bf
Neil Stone <neil.stone@gmail.com>
2021-05-31 19:14:33 +0100

    post.sh: Add Netdata monitoring

--- post.sh
+++ post.sh
@@ -8,3 +8,9 @@ mv wp-cli.phar /usr/local/bin/wp
 
 curl -O https://raw.githubusercontent.com/wp-cli/wp-cli/v2.4.0/utils/wp-completion.bash
 mv wp-completion.bash /etc/bash_completion.d/
+
+# Netdata monitoring
+curl -s https://packagecloud.io/install/repositories/netdata/netdata/script.deb.sh | bash
+apt install -y netdata
+netdata-claim.sh -token=piBpU6UlFvRHCrLybO8oaqY3yBcIvo7OleJSX6gFHMAwD1ijLkAtA4RKzYFhnqmk8dDc3ctxP27L-Wj-xQPC9SeQqYRGkgLgjUIM8lYE4f7bpKLQOU3ls-Gs69i3OMw13r-24VM -rooms=b16a5c65-1e08-4a38-80d1-dbd8685cf31d -url=https://app.netdata.cloud
+/etc/init.d/netdata restart
