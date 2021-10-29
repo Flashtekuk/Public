47ab84136ddc6f33f0b2a7bda63efdce8510bfa7
Neil Stone <neil.stone@loadbalancer.org>
2021-10-29 13:11:32 +0100

    READSME.md - updated with upload_license.sh upload_license.sh - Initial commit upload_ssh.sh - Marked +x

---
+++ LB/upload_license.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERNAME=loadbalancer
+PASSWORD=loadbalancer
+LB=${1}
+LICENSE=${2}
+
+if [ ${#} -ne 2 ]; then
+	echo "Usage: $0 IP /path/to/License.lbk"
+	exit 69
+fi
+
+curl -k -X POST -u ${USERNAME}:${PASSWORD} \
+	--form "submit=file" \
+	--form "value=@${LICENSE}" \
+	--silent
+	 https://${IP}:9443/lbadmin/config/upgrade.php
