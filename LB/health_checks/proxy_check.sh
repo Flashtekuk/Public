020009589285857445e3693c926da18710da262e
Neil Stone <neil.stone@gmail.com>
2021-11-24 19:43:14 +0000

    proxy_check.sh - Tidy up v1.2 changes

--- LB/health_checks/proxy_check.sh
+++ LB/health_checks/proxy_check.sh
@@ -8,6 +8,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 # v1.0 - 2021-11-24 - Neil Stone <support@loadbalancer.org> - Initial creation.
 # v1.1 - 2021-11-24 - Neil Stone <support@loadbalancer.org> - Improve handling of multiple site checks.
 # v1.2 - 2021-11-24 - Neil Stone <support@loadbalancer.org> - Exit loop on first success.
+# v1.3 - 2021-11-24 - Neil Stone <support@loadbalancer.org> - Tidy up v1.2 changes.
 #
 #########################################################################################################
 
@@ -18,7 +19,7 @@ fi
 
 # user defined variables
 CURLOPTS=""
-# CURLOPTS="--haproxy-protocol" # Set this if proxy protocol in play on real server - Requires cURL v7.60.0 (Loadbalancer.org appliance v8.6) or newer.
+CURLOPTS="--haproxy-protocol" # Set this if proxy protocol in play on real server - Requires cURL v7.60.0 (Loadbalancer.org appliance v8.6) or newer.
 # Set the following space separated list of the pages to check
 PAGES="www.bbc.co.uk www.microsoft.com www.google.com www.loadbalancer.org"
 # ${CHECK_STATUS} is the HTTP status that must be returned by at least one of the ${PAGES}
@@ -37,24 +38,16 @@ else
 fi
 
 # Program starts
-STATUS=0
-COUNT=0
 
 for PAGE in ${PAGES}
 do
-	CURL_OUT=$(curl ${CURLOPTS} --output /dev/null --write-out "%{http_code}" \
+	CURL_OUT=$(curl ${CURLOPTS} --output /dev/null \
+		--write-out "%{http_code}" \
 		--header 'Cache-Control: no-cache' \
 		--head --silent --max-time 3 \
 		--url http://${PAGE}/ \
 		--proxy ${REAL_IP}:${REAL_PORT})
 	echo ${CURL_OUT} | grep -q -e "${CHECK_STATUS}" && exit 0
-	let STATUS=${STATUS}+${?}
-	let COUNT++
 done
 
-if [ ${COUNT} -gt ${STATUS} ]
-then
-    exit 0 # At least one returned the ${CHECK_STATUS} value.
-else
-    exit 42 # NONE returned the ${CHECK_STATUS} value.
-fi
+exit 42
