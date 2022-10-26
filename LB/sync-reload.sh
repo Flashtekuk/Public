2c1d8109a48faf236d94a2801e863c193cabb68f
Neil Stone <neil.stone@gmail.com>
2022-10-26 21:28:14 +0100

    sync-reload.sh - Initial commit

---
+++ LB/sync-reload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+## FILES is a space separated list of files to sync
+FILES="/etc/haproxy/neuanalytics_allowed2.txt"
+
+## Shouldn't need to edit below here.
+CHANGE=0
+
+for FILE in ${FILES}; do
+    md5sum ${FILE} > ${FILE}.md5-1
+    cmp -s ${FILE}.md5-1 ${FILE}.md5-2
+    EC=${?}
+    if [ ${EC} -ne 0 ]; then
+        scp ${FILE} lbslave:${FILE} > /dev/null
+        let CHANGE=${CHANGE}+1
+    cp -f ${FILE}.md5-1 ${FILE}.md5-2
+    fi
+done
+
+if [ ${CHANGE} -ne 0 ]; then
+    ## Restart locally
+    service haproxy reload > /dev/null
+    ## Restart on secondary
+    ssh lbslave -- service haproxy reload > /dev/null
+fi
