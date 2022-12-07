72fa3bf60cb8f5511ba0d2ac0037955e81ea6d48
Neil Stone <neil.stone@gmail.com>
2022-12-07 20:18:27 +0000

    Exit clean is desired state

--- LB/health_checks/84-80-check.sh
+++ LB/health_checks/84-80-check.sh
@@ -8,6 +8,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # v0.1 - Neil Stone <support@loadbalancer.org> - 20221206 - Initial write
 # v0.2 - Neil Stone <support@loadbalancer.org> - 20221207 - Updated output from curl stderr to stdout
 # v0.3 - Neil Stone <support@loadbalancer.org> - 20221207 - Updated output from curl to output to tee and append to log
+# v0.4 - Neil Stone <support@loadbalancer.org> - 20221207 - Exit 0 on desired http exit code
 #
 #########################################################################################################################
 
@@ -19,10 +20,16 @@ RIP=${3}
 RPT=${4}
 RPATH="admin/healthcheck"
 URL="http://${RIP}:${RPT}/${RPATH}"
+WANTEDCODE="200"
 
 echo "####################################################################################################################################" >> ${LOG}
 echo "$(date --rfc-3339=seconds) - health check of ${URL} started" >> ${LOG}
 EC=$(curl --silent --head --write-out '%{http_code}\n' ${URL} 2>&1 | tee -a ${LOG})
 echo "$(date --rfc-3339=seconds) - health check of ${URL} finished - State: ${EC}" >> ${LOG}
+
+if [ ${EC} -eq ${WANTEDCODE} ]; then
+	echo "Exit state matched ${WANTEDCODE} - Exiting 0" >> ${LOG}
+	exit 0
+fi
 
 exit ${EC}
