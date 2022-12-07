12b917ed7cd8c0519bcc35db296f0326892ba51d
Neil Stone <neil.stone@gmail.com>
2022-12-07 21:26:59 +0000

    More checks, logging more things

--- LB/health_checks/84-80-check.sh
+++ LB/health_checks/84-80-check.sh
@@ -11,6 +11,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # v0.4 - Neil Stone <support@loadbalancer.org> - 20221207 - Exit 0 on desired http exit code
 # v0.5 - Neil Stone <support@loadbalancer.org> - 20221207 - Additional checks for port open, reorder a few bits, include timeouts
 # v0.6 - Neil Stone <support@loadbalancer.org> - 20221207 - Redirect output of nc to /dev/null
+# v0.7 - Neil Stone <support@loadbalancer.org> - 20221207 - More checks, logging more things
 #
 ###################################################################################################################################
 
@@ -30,13 +31,17 @@ RPATH="admin/healthcheck"
 URL="http://${RIP}:${CHECK_PORT}/${RPATH}"
 WANTEDCODE="200"
 
+echo "####################################################################################################################################" >> ${LOG}
+
 nc -zvn -w ${TIMEOUT} ${RIP} ${CHECK_PORT} > /dev/null 2>&1
 NC_EC=${?}
 if [ ${NC_EC} -ne 0 ]; then
+	echo "$(date --rfc-3339=seconds) - nc failed to connect to ${RIP}:${CHECK_PORT} within ${TIMEOUT} seconds" >> ${LOG}
 	exit 199
+else
+	echo "$(date --rfc-3339=seconds) - nc successfully connected to ${RIP}:${CHECK_PORT} within ${TIMEOUT} seconds" >> ${LOG}
 fi
 
-echo "####################################################################################################################################" >> ${LOG}
 echo "$(date --rfc-3339=seconds) - health check of ${URL} started" >> ${LOG}
 CURL_EC=$(curl --max-time ${TIMEOUT} -o /dev/null --silent --head --write-out '%{http_code}\n' ${URL} 2>&1 | tee -a ${LOG})
 echo "$(date --rfc-3339=seconds) - health check of ${URL} finished - State: ${CURL_EC}" >> ${LOG}
@@ -44,7 +49,8 @@ echo "$(date --rfc-3339=seconds) - health check of ${URL} finished - State: ${CU
 if [ ${CURL_EC} -eq ${WANTEDCODE} ]; then
 	echo "Exit state matched ${WANTEDCODE} - Exiting 0" >> ${LOG}
 	exit 0
+else
+	echo "Exit state did not match ${WANTEDCODE} - Exiting 0" >> ${LOG}
+	exit ${CURL_EC}
 fi
-
-exit ${CURL_EC}
 
