5fbd400bccd6429d64705b26a83a143623a7749c
Neil Stone <neil.stone@gmail.com>
2022-12-07 20:12:08 +0000

    Updated output from curl to output to tee and append to log

--- LB/health_checks/84-80-check.sh
+++ LB/health_checks/84-80-check.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-######################################################################################################
+########################################################################################################################
 #
 # 84-80-check.sh
 #
 # v0.1 - Neil Stone <support@loadbalancer.org> - 20221206 - Initial write
 # v0.2 - Neil Stone <support@loadbalancer.org> - 20221207 - Updated output from curl stderr to stdout
+# v0.3 - Neil Stone <support@loadbalancer.org> - 20221207 - Updated output from curl to output to tee and append to log
 #
-#######################################################################################################
+#########################################################################################################################
 
 LOG=/var/log/L4-v84_80-healthcheck.log
 
@@ -21,7 +22,7 @@ URL="http://${RIP}:${RPT}/${RPATH}"
 
 echo "####################################################################################################################################" >> ${LOG}
 echo "$(date --rfc-3339=seconds) - health check of ${URL} started" >> ${LOG}
-curl --silent --head --write-out '%{http_code}\n' ${URL} 2>&1 >> ${LOG} ; EC=${?}
+EC=$(curl --silent --head --write-out '%{http_code}\n' ${URL} 2>&1 | tee -a ${LOG})
 echo "$(date --rfc-3339=seconds) - health check of ${URL} finished - State: ${EC}" >> ${LOG}
 
 exit ${EC}
