f72e728f34fd31b0d8de51ecf6504d515b0ffce8
Neil Stone <neil.stone@gmail.com>
2022-12-08 09:59:51 +0000

    Initial commit

---
+++ LB/fixes/fork-fix.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PATH=/bin:/sbin
+
+########################################################################################
+#
+# fork-fix.sh - Script to work around XML to .cf bug for ldirectord 'fork' setting
+#
+# v0.1 - Neil Stone <support@loadbalancer.org> - 20221208 - Initial commit
+#
+########################################################################################
+
+CF=/etc/ha.d/conf/ldirectord.cf
+
+WANTFORK=$(grep 'fork' /etc/loadbalancer.org/lb_config.xml |cut -f2 -d\<|cut -f2 -d\>) 
+ISFORK=$(grep ^fork ${CF} | cut -f2 -d\= )
+
+if [ ${WANTFORK} != ${ISFORK} ]; then
+   sed -i.bak s:fork=${ISFORK}:fork=${WANTFORK}: ${CF}
+   echo -n $(md5sum ${CF} |cut -f 1 -d\  ) > /etc/ha.d/conf/ldirectord.md5
+   service ldirectord restart
+fi
+
+exit 0
