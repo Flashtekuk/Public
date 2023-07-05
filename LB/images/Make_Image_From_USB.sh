e7a6ceb69a9ad8802e5bf005b65a38d0e149f0e9
Neil Stone <neil.stone@loadbalancer.org>
2023-07-05 13:24:37 +0100

    Make_Image_From_USB.sh - Correct some echo statements

--- LB/images/Make_Image_From_USB.sh
+++ LB/images/Make_Image_From_USB.sh
@@ -7,10 +7,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 IMG_NAME=image.img
 
+if [ ${UID} -ne 0 ]; then
+    echo "ERROR: ${0} must be run as root user"
+    exit 7
+fi
+
 # Check for correct number of args
 if [ ${#} -ne 1 ]; then
     echo "Usage: ${0} DEVNAME"
-    echo "where DEVNAME is the device root node (eg: /dev/sdc)"
+    echo "where DEVNAME is the device root node (eg: for /dev/sdc provide 'sdc')"
     exit 2
 else
     DISK=${1}
@@ -34,9 +39,7 @@ echo "##########################################################################
 # Set vars to file sizes.
 PART1_SIZE=$(du --apparent-size -B 512 ${DISK}1 | awk '{print $1}' )
 PART2_SIZE=$(du --apparent-size -B 512 ${DISK}2 | awk '{print $1}' )
-
 DISK_SIZE=$(( ${PART1_SIZE} + ${PART2_SIZE} ))
-
 IMG_SIZE=$(( (${DISK_SIZE} + 16 + 10240) * 512 ))
 
 echo "Creating a blank file to be used for the image."
@@ -52,18 +55,15 @@ echo "Replicating /dev/${DISK} partition table to ${LO_IMG}"
 sgdisk --replicate=${LO_IMG} /dev/${DISK}
 echo "##############################################################################"
 
-echo "Copying ${DISK}1 to ${LO_IMG}1"
+echo "Copying ${DISK}1 to ${LO_IMG}p1"
 dd status=progress if=${DISK}1 of=${LO_IMG}p1
-echo "Copying ${DISK}2 to ${LO_IMG}2"
+echo "Copying ${DISK}2 to ${LO_IMG}p2"
 dd status=progress if=${DISK}2 of=${LO_IMG}p2
 sync
 echo "##############################################################################"
 
 echo "Cleaning up..."
-echo "##############################################################################"
 losetup -d ${LO_IMG}
 rm -vf ${DISK}1 ${DISK}2
-
 echo "####################################### END ##################################"
-
 exit 0
