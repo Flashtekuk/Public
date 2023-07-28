53c480d6039065115b89ef63822be3ed5e780972
Neil Stone <neil.stone@loadbalancer.org>
2023-07-28 14:30:34 +0100

    Added support for detecting based on haproxy.cfg contents

--- LB/scripts/cipherscan.sh
+++ LB/scripts/cipherscan.sh
@@ -5,8 +5,9 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 #  cipherscan.sh - Bash script to check which ciphers are available on a target system using both available versions of openssl on a Loadbalancer.org appliance.
 #  Inspiration from https://www.ise.io/blog/archives/using-openssl-determine-ciphers-enabled-server/
 #
-#  v0.1 - 2023-07-23 - Initial write - Neil Stone <support@loadbalancer.org>
-#  v0.2 - 2023-07-23 - Added basic check for port contactability, renamed a few vaiables - Neil Stone <support@loadbalancer.org>
+#  v0.1 - 2023-07-27 - Initial write - Neil Stone <support@loadbalancer.org>
+#  v0.2 - 2023-07-27 - Added basic check for port contactability, renamed a few vaiables - Neil Stone <support@loadbalancer.org>
+#  v0.3 - 2023-07-28 - Added support for check support from haproxy.cfg - Neil Stone <support@loadbalancer.org>
 #
 ######################
 
@@ -29,12 +30,27 @@ for OPENSSL in /usr/local/bin/openssl /usr/bin/openssl; do
     echo "#################################"
     echo "Scanning - ${SERVER}:${PORT}" with ${OPENSSL}
     echo "#################################"
-
-    for VER in ssl2 ssl3 tls1 tls1_1 tls1_2 tls1_3; do
+    for VER in ssl2 ssl3 tls1 tls1_1 tls1_2; do
         for CIPHER in $(${OPENSSL} ciphers 'ALL:eNULL' | tr ':' ' '); do
             ${OPENSSL} s_client -connect ${SERVER}:${PORT} -cipher ${CIPHER} -${VER} < /dev/null > /dev/null 2>&1 && echo -e "${VER}:\t${CIPHER}"
         done
     done
+done
+
+for OPENSSL in /usr/local/bin/openssl /usr/bin/openssl; do
+    echo "#################################"
+    echo "Scanning target system for ciphers supported from haproxy.cfg with ${OPENSSL}"
+    echo "#################################"
+    for VER in ssl2 ssl3 tls1 tls1_1 tls1_2; do
+        for CIPHER in $(awk -F\" '/ssl-default-server-ciphers\ /{print $2}' /etc/haproxy/haproxy.cfg |  tr ':' ' '); do
+            ${OPENSSL} s_client -connect ${SERVER}:${PORT} -cipher ${CIPHER} -${VER} < /dev/null > /dev/null 2>&1 && echo -e "${VER}:\t${CIPHER}"
+        done
+    for VER in tls1_3; do
+        for CIPHERSUITE in $(awk -F\" '/ssl-default-server-ciphersuites\ /{print $2}' /etc/haproxy/haproxy.cfg |  tr ':' ' '); do
+            ${OPENSSL} s_client -connect ${SERVER}:${PORT} -cipher ${CIPHERSUITE} -${VER} < /dev/null > /dev/null 2>&1 && echo -e "${VER}:\t${CIPHERSUITE}"
+        done
+    done
+done
 done
 
 echo "#################################"
