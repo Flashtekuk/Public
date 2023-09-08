c1780714247e48b5cb3f7668520b96f3720e1661
Neil Stone <neil.stone@loadbalancer.org>
2023-09-08 10:46:46 +0100

    haproxy-nossl.sh - Add header info

--- LB/scripts/haproxy-nossl.sh
+++ LB/scripts/haproxy-nossl.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-set -x
+
+################
+#
+# haproxy-nossl.sh - Bash script to remove the 'ssl-default' options set by v8.9.1
+#
+# v1.0 - 2023-07-27 - Initial write - Neil Stone <support@loadbalancer.org>
+#
+################
 
 sed -i.bak '/ssl-default*/d' /etc/haproxy/haproxy.cfg
 md5sum /etc/haproxy/haproxy.cfg | cut -f 1 -d\  > /etc/haproxy/haproxy.md5
