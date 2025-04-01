978bef44d0e703694a5533a68d3133b0d4da9893
Neil Stone <neil.stone@loadbalancer.org>
2025-04-01 10:04:06 +0100

    configure.sh - Update for ENT-403

--- LB/scripts/build/configure.sh
+++ LB/scripts/build/configure.sh
@@ -14,9 +14,10 @@ else
 	yes | lbinit
 	yes | lbrestore
 	lbfirstboot
-	lb_net_setup.php ${IP}/24 ${GW} ${DNS}
+#	lb_net_setup.php ${IP}/24 ${GW} ${DNS}
+	lb_net_setup.php --ip ${IP}/24 --gateway ${GW} --dns ${DNS} --password loadbalancer
 	ip a a ${IP}/24 dev eth0
-	htpasswd -b /etc/loadbalancer.org/passwords loadbalancer loadbalancer
+#	htpasswd -b /etc/loadbalancer.org/passwords loadbalancer loadbalancer
 	lbconsoleenable
 	reboot
 	rm -f /root/configure.sh
