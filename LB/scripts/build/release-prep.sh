4c65a3ec33de5cb24182f236d75abcf49408b466
Neil Stone <neil.stone@loadbalancer.org>
2025-06-27 12:50:47 +0100

    .

--- LB/scripts/build/release-prep.sh
+++ LB/scripts/build/release-prep.sh
@@ -22,34 +22,27 @@ else
    exit 42
 fi
 
-#(use chattr -i <file> to allow model.* to be modified)
+# Tidy up filesystem
    rm -rf /var/www/html_*
-   chattr -i /var/www/html/lbadmin/inc/model.php
-
-#And /etc/httpd_* folders - i.e. archived httpd folders
    rm -rf /etc/httpd_*
-
-#And /tmp/*
    rm -rf /tmp/*
-
-#Make sure Debug is False in /var/www/html/lbadmin/inc/lbadmin_config.*
-   grep \$DEBUG /var/www/html/lbadmin/inc/lbadmin_config.php | grep False || 'echo "DEBUG set, exiting" ; exit 69'
-
-#Remove any local XML and firewall script backup files
    rm -f /etc/loadbalancer.org/lb_config.xml.{old,slave}
-
-#Remove any old cert files in /etc/loadbalancer.org/certs/
    rm -rf /etc/loadbalancer.org/certs/[!server.???][!backup]
 
-#Remove any entries in /root/.ssh/known_hosts etc
+   chattr -i /var/www/html/lbadmin/inc/model.php
+
+# Make sure Debug is False in /var/www/html/lbadmin/inc/lbadmin_config.*
+   grep \$DEBUG /var/www/html/lbadmin/inc/lbadmin_config.php | grep False || 'echo "DEBUG set, exiting" ; exit 69'
+
+# Remove any entries in /root/.ssh/
    truncate -s 0 /root/.ssh/known_hosts
    truncate -s 0 /root/.ssh/authorized_keys
    truncate -s 0 /root/.ssh/authorized_keys2
 
-#Run the command
+# Clean up yum
    yum clean all
 
-#Now run the following commands(make sure to log in at the console before using lbrestore):
+# Apply license and perform primary log clean
 
    yes | ${LICENSE}
    lblogclean
@@ -67,9 +60,9 @@ done
 
 
 # Final clear up:
-yes | lbinit
-lblogclean
-echo "Next, run:- history -c ; lbcleanboot"
+   yes | lbinit
+   lblogclean
+   echo "Next, run:- history -c ; lbcleanboot"
 
 # Commit suicide
-#rm -fv ${0}
+   rm -fv ${0}
